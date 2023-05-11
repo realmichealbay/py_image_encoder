9de920079a6dd01d144690aad68b4cfb00bd5591
Victor Johnson <95552161+realmichealbay@users.noreply.github.com>
2023-05-11 15:26:44 -0500

    On branch master  Changes to be committed: 	new file:   .gitignore 	modified:   main.py 	deleted:    msg.txt

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import os 
 
-textI = input("Select Encode or Decode")
+textI = input("Select Encode or Decode: ")
 
 if textI.strip().lower() == "encode":
     # encode
     message = input("Enter Message to be Encoded: ")
     with open("msg.txt","w") as file:
-        file.write(message)
+        file.write(f"MESSAGESTART{message}")
         file.close()
-    with open("shoebill.png","rb") as file:
-        base_image = file.read()
-        print(base_image)
-
-
-
+    os.system("copy /b shoebill.png + msg.txt shoebill_encoded.png")
+    
     try:
         os.remove("msg.txt")
     except:
@@ -21,6 +17,17 @@ if textI.strip().lower() == "encode":
         
 elif textI.strip().lower() == "decode":
     # decode
-    print("wip")
+    with open("shoebill.png","rb") as file:
+        base_image = file.read()
+        file.close()
+    with open("shoebill_encoded.png","rb") as file:
+        encoded_image = str(file.read())
+        file.close() 
+    
+    index = encoded_image.find("MESSAGESTART") + 12
+    print(encoded_image[index:-1])
+
+    
 else:
-    raise ValueError("Has to be encode or decode")
+    raise ValueError("Has to be encode or decode (this is technically not an error dont say there was an error i could of just used a print statement)")
+    
